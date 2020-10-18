4f452d77708204a86e53532a91a694066e2d3db9
dengzhifeng <398577351@qq.com>
2020-10-18 17:49:54 +0800

    vue

---
+++ src/vue3/vue3/typescript/1.基本数据类型.ts
@@ -0,0 +1,9 @@
+/*
+ * @description:
+ * @author: steve.deng
+ * @Date: 2020-10-12 20:51:03
+ * @LastEditors: steve.deng
+ * @LastEditTime: 2020-10-12 20:51:21
+ */
+let num: number = 1;
+console.log(num);

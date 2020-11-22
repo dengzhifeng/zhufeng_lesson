83f46392a0d6b73d89e2b140babde573e0f39bb6
邓志锋 <398577351@qq.com>
2020-11-22 22:54:25 +0800

    update: fix

---
+++ src/vue3.0_ts/src/typings/home.ts
@@ -0,0 +1,44 @@
+/*
+ * @description:
+ * @author: steve.deng
+ * @Date: 2020-11-22 12:08:22
+ * @LastEditors: steve.deng
+ * @LastEditTime: 2020-11-22 14:31:36
+ */
+
+// 轮播图对象接口
+export interface ISlider {
+    url: string;
+}
+
+// 课程对象接口
+export interface ILesson {
+    title: string;
+    video: string;
+    poster: string;
+    price: number;
+    category?: string;
+}
+
+// 课程信息接口
+export interface ILessons {
+    hasMore: boolean; // 有没有更多数据
+    loading: boolean; // 默认没有正在加载
+    offset: number;
+    limit: number;
+    list: ILesson[]; // 页面显示的列表
+}
+
+// 首页状态接口
+export interface IHomeState {
+    currentCategory: CATEGORY_TYPES;
+    sliders: ISlider[];
+    lessons: ILessons;
+}
+
+export enum CATEGORY_TYPES {
+    ALL,
+    REACT,
+    VUE,
+    NODE
+}
